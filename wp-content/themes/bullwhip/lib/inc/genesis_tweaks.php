2995a5c2b4289d1f1609996d44aa8025cb4e3188
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-08-22 14:47:18 -0400

    change image size

--- wp-content/themes/bullwhip/lib/inc/genesis_tweaks.php
+++ wp-content/themes/bullwhip/lib/inc/genesis_tweaks.php
@@ -126,24 +126,3 @@ function msd_new_custom_layout_logic() {
 		add_action( 'genesis_after_content', 'genesis_get_sidebar_alt');
 	}
 }
-/** Add new image sizes */
-add_image_size( 'post-image', 670, 242, TRUE ); //image to float at the top of the post. Reversed Out does these a lot.
-
-/* Manipulate the featured image */
-add_action( 'genesis_before_post', 'msd_post_image', 8 );
-function msd_post_image() {
-	global $post;
-   	//setup thumbnail image args to be used with genesis_get_image();
-	$size = 'post-image'; // Change this to whatever add_image_size you want
-	$default_attr = array(
-			'class' => "alignnone attachment-$size $size",
-			'alt'   => $post->post_title,
-			'title' => $post->post_title,
-	);
-
-	// This is the most important part!  Checks to see if the post has a Post Thumbnail assigned to it. You can delete the if conditional if you want and assume that there will always be a thumbnail
-	if ( has_post_thumbnail() && is_page() ) {
-		printf( '<a title="%s" href="%s">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), genesis_get_image( array( 'size' => $size, 'attr' => $default_attr ) ) );
-	}
-
-}
--- wp-content/themes/bullwhip/lib/inc/media.php
+++ wp-content/themes/bullwhip/lib/inc/media.php
@@ -22,3 +22,25 @@ function msd_insert_custom_image_sizes( $sizes ) {
 }
 }
 add_filter( 'image_size_names_choose', 'msd_insert_custom_image_sizes' );
+
+/** Add new image sizes */
+add_image_size( 'post-image', 740, 280, TRUE ); //image to float at the top of the post. Reversed Out does these a lot.
+
+/* Manipulate the featured image */
+add_action( 'genesis_before_post', 'msd_post_image', 8 );
+function msd_post_image() {
+    global $post;
+    //setup thumbnail image args to be used with genesis_get_image();
+    $size = 'post-image'; // Change this to whatever add_image_size you want
+    $default_attr = array(
+            'class' => "alignnone attachment-$size $size",
+            'alt'   => $post->post_title,
+            'title' => $post->post_title,
+    );
+
+    // This is the most important part!  Checks to see if the post has a Post Thumbnail assigned to it. You can delete the if conditional if you want and assume that there will always be a thumbnail
+    if ( has_post_thumbnail() && is_page() ) {
+        printf( '<a title="%s" href="%s">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), genesis_get_image( array( 'size' => $size, 'attr' => $default_attr ) ) );
+    }
+
+}
